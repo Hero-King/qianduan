cf865150342737b730b7b032c83bf00a643ae76b
wangjunjie <SV00249433@Svolt.cn>
2023-05-08 18:19:45 +0800

    g6-stu

--- public/myUtil.js
+++ public/myUtil.js
@@ -13,3 +13,20 @@ var logHandler = {
     return target[key];
   },
 };
+
+/**
+ * 根据文件名判断是否是图片格式
+ * @param {string} name
+ * @returns Boolean
+ */
+function checkImgType(name) {
+  if (
+    !/\.(bmp|jpg|png|tif|gif|pcx|tga|exif|fpx|svg|psd|cdr|pcd|dxf|ufo|eps|ai|raw|WMF|webp|jpeg)$/.test(
+      name.toLowerCase()
+    )
+  ) {
+    return false;
+  } else {
+    return true;
+  }
+}
